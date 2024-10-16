d137723aa0d2478d68c9690eaa5505ac2f4d07f7
Alexey Bolvonovich <alexeybolv@gmail.com>
2024-10-16 02:46:40 +0200

    Add template views

---
+++ Tip Calculator/Tip Calculator/Views/BillInputView.swift
@@ -0,0 +1,24 @@
+//
+//  BillInputView.swift
+//  Tip Calculator
+//
+//  Created by Alexey Bolvonovich on 16/10/2024.
+//
+
+import UIKit
+
+class BillInputView: UIView {
+    
+    init() {
+        super.init(frame: .zero)
+        layout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func layout() {
+        backgroundColor = .blue
+    }
+}
