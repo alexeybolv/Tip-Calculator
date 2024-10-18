59897dee073903ea7504afb336036510a0c198b6
Alexey Bolvonovich <alexeybolv@gmail.com>
2024-10-18 12:52:45 +0200

    Configure header view

--- Tip Calculator/Tip Calculator/Views/HeaderView.swift
+++ Tip Calculator/Tip Calculator/Views/HeaderView.swift
@@ -9,6 +9,38 @@ import UIKit
 
 class HeaderView: UIView {
     
+    private let topLabel: UILabel = {
+        LabelFactory.build(
+            text: nil,
+            font: ThemeFont.bold(ofSize: 18)
+        )
+    }()
+    
+    private let bottomLabel: UILabel = {
+        LabelFactory.build(
+            text: nil,
+            font: ThemeFont.regular(ofSize: 16)
+        )
+    }()
+    
+    private let topSpacerView = UIView()
+    private let bottomSpacerView = UIView()
+    
+    private lazy var stackView: UIStackView = {
+        let stackView = UIStackView(
+            arrangedSubviews: [
+                topSpacerView,
+                topLabel,
+                bottomLabel,
+                bottomSpacerView
+            ]
+        )
+        stackView.axis = .vertical
+        stackView.spacing = -4
+        stackView.alignment = .leading
+        return stackView
+    }()
+    
     init () {
         super.init(frame: .zero)
         layout()
@@ -19,5 +51,17 @@ class HeaderView: UIView {
     }
     
     func layout() {
+        addSubview(stackView)
+        stackView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+        topSpacerView.snp.makeConstraints {
+            $0.height.equalTo(bottomSpacerView)
+        }
+    }
+    
+    func configure(topText: String, bottomText: String) {
+        topLabel.text = topText
+        bottomLabel.text = bottomText
     }
 }
