1ff2a47f52bf546cc44a23b47e03af93b8a34818
Alexey Bolvonovich <alexeybolv@gmail.com>
2024-10-25 02:59:09 +0200

    Add more tests for view model

--- Tip Calculator/Tip CalculatorTests/Tip_CalculatorTests.swift
+++ Tip Calculator/Tip CalculatorTests/Tip_CalculatorTests.swift
@@ -42,7 +42,51 @@ final class Tip_CalculatorTests: XCTestCase {
             XCTAssertEqual(result.totalTip, 0)
         }.store(in: &cancellables)
     }
+    
+    func testResultWithoutTipForTwoPerson() {
+        let bill: Double = 100
+        let tip: Tip = .none
+        let split: Int = 2
+        let input = buildInput(bill: bill, tip: tip, split: split)
         
+        let output = sut.transform(input: input)
+        
+        output.updateViewPublisher.sink { result in
+            XCTAssertEqual(result.amountPerPerson, 50)
+            XCTAssertEqual(result.totalBill, 100)
+            XCTAssertEqual(result.totalTip, 0)
+        }.store(in: &cancellables)
+    }
+    
+    func testResultWithTenPercentTipForTwoPerson() {
+        let bill: Double = 100
+        let tip: Tip = .tenPercent
+        let split: Int = 2
+        let input = buildInput(bill: bill, tip: tip, split: split)
+        
+        let output = sut.transform(input: input)
+        
+        output.updateViewPublisher.sink { result in
+            XCTAssertEqual(result.amountPerPerson, 55)
+            XCTAssertEqual(result.totalBill, 110)
+            XCTAssertEqual(result.totalTip, 10)
+        }.store(in: &cancellables)
+    }
+    
+    func testResultWithCustomTipForFourPerson() {
+        let bill: Double = 200
+        let tip: Tip = .custom(value: 201)
+        let split: Int = 4
+        let input = buildInput(bill: bill, tip: tip, split: split)
+        
+        let output = sut.transform(input: input)
+        
+        output.updateViewPublisher.sink { result in
+            XCTAssertEqual(result.amountPerPerson, 100.25)
+            XCTAssertEqual(result.totalBill, 401)
+            XCTAssertEqual(result.totalTip, 201)
+        }.store(in: &cancellables)
+    }
     
     private func buildInput(bill: Double, tip: Tip, split: Int) -> CalculatorViewModel.Input {
         return .init(
@@ -52,5 +96,4 @@ final class Tip_CalculatorTests: XCTestCase {
             logoViewTapPublisher: logoViewTapSubject.eraseToAnyPublisher()
         )
     }
-    
 }
